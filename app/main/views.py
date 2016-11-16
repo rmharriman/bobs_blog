43cc749da3da21811699c55542b961a9804eee6d
Robert Harriman <rmharriman@gmail.com>
2016-11-15 19:18:24 -0500

    Added user profile forms, view functions and template

--- app/main/views.py
+++ app/main/views.py
@@ -1,38 +1,16 @@
 from flask import render_template, session, redirect, url_for, current_app
+from flask_login import login_required, current_user
 from . import main
-from .forms import NameForm
+from .forms import EditProfileForm, EditProfileAdminform
 from .. import db
-from ..models import User
-from ..email import send_email
+from ..models import User, Role
+from ..decorators import admin_required
 
 
 # Important to remember route decorator comes from the bp not app
 @main.route("/", methods=["GET", "POST"])
 def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()
-        if user is None:
-            user = User(username=form.name.data)
-            db.session.add(user)
-            session["known"] = False
-            if current_app.config.get("BLOG_ADMIN"):
-                send_email(current_app.config.get("BLOG_ADMIN"), "New User",
-                           'mail/new_user', user=user)
-        else:
-            session["known"] = True
-        session["name"] = form.name.data
-        # URL for function arg is also different with bp's
-        # No longer can routes be accessed with just name of the view function
-        # Flask applies a namespace to all endpoints in a bp (namespace is name of bp)
-        ## so multiple bp's can be defined
-        # index() view func is registered as main.index
-        # shorthand is supported tho so .index works - in this case bp for current request is used
-        # redirects in same bp can use shorthand, across bp's must use full namespaced endpoint
-        return redirect(url_for(".index"))
-    return render_template("index.html",
-                           form=form, name=session.get("name"),
-                           known=session.get("known", False))
+    return render_template("index.html")
 
 @main.route("/user/<username>")
 def user(username):
@@ -40,3 +18,45 @@ def user(username):
     if user is None:
         abort(404)
     return render_template("user.html", user=user)
+
+@main.route("/edit-profile", methods=["GET", "POST"])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.name.location
+        current_user.about_me = form.name.about_me.data
+        db.session.add(current_user)
+        flash("Your profile has been updated.")
+        return redirect(url_for(".user", username=current_user.username))
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+    return render_template("edit_profile.html", form=form)
+
+@main.route("/edit-profile/<int:id>", methods=["GET", "POST"])
+@login_required
+@admin_required
+def edit_profile_admin(id):
+    user = User.query.get_or_404(id)
+    form = EditProfileAdminForm(user=user)
+    if form.validate_on_submit():
+        user.email = form.email.data
+        user.username = form.username.data
+        user.confirmed = form.confirmed.data
+        user.role = Role.query.get(form.role.data)
+        user.name = form.name.data
+        user.location = form.location.data
+        user.about_me = form.about_me.data
+        db.session.add(user)
+        flash("The profile has been updated.")
+        return redirect(url_for(".user", username=user.username)
+    form.email.data = user.email
+    form.username.data = user.username
+    form.confirmed.data = user.confirmed
+    form.role.data = user.role_id
+    form.name.data = user.name
+    form.location.data = user.location
+    form.about_me.data = user.about_me
+    return render_template("edit_profile.html", form=form, user=user)
