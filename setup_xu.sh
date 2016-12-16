995ccdac4023bcf4b40da2173c2b7e9f7303d2a7
Rob <rob@uberresearch.com>
2016-12-16 14:43:33 -0500

    Bash script to export variables needed to setup application

---
+++ setup_xu.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+export MIGRATIONS=/home/rmharriman/PycharmProjects/bobs_blog/migrations/
+export BLOG_HOME=/home/rmharriman/PycharmProjects/bobs_blog
+export BLOG_VENV="$BLOG_HOME/bbb"
+export BLOG_ADMIN=rmharriman@gmail.com
+export DEV_DATABASE_URI=sqlite:////tmp/dev.db
+export TEST_DATABASE_URI=sqlite:////tmp/test.db
