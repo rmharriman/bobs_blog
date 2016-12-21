c2144053bd8b1a627d8b3fa7024b661aa4ffcb5b
Rob <rob@uberresearch.com>
2016-12-21 12:53:51 -0500

    Fixed typo in basic auth header, added new user endpoint imports

--- app/api_1_0/users.py
+++ app/api_1_0/users.py
@@ -1,12 +1,10 @@
-from flask import jsonify, request, g, abort, url_for, current_app
-from .. import db
-from .decorators import permission_required
-from .errors import forbidden
+from flask import jsonify, request, current_app, url_for
 from . import api
-from ..models import User
+from ..models import User, Post
 
 
 @api.route("/users/<int:id>")
 def get_user(id):
     user = User.query.get_or_404(id)
     return jsonify(user.to_json())
+
