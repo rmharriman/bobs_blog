c751308da9f1b98dff513e165d0b7ffe562d7907
Rob <rob@uberresearch.com>
2016-12-21 10:50:23 -0500

    Fixed typo in posts to_json method

--- app/api_1_0/errors.py
+++ app/api_1_0/errors.py
@@ -21,7 +21,7 @@ def forbidden(message):
     return response
 
 
-# Decorator takes an Exception class, and is invoked everytime the exception is raised
+# Decorator takes an Exception class, and is invoked every time the exception is raised
 @api.errorhandler(ValidationError)
 def validation_error(e):
     return bad_request(e.args[0])
--- app/models.py
+++ app/models.py
@@ -332,13 +332,13 @@ class Post(db.Model):
     def to_json(self):
         # Comment count is "made-up" attribute. Shows a technique to add resource attributes without storing them.
         json_post = {
-            "url" : url_for("api.get_post", id=self.id, _external=True),
-            "body" : self.body,
-            "html_body" : self.html_body,
-            "timestamp" : self.timestamp,
-            "author" : url_for("api.get_user", id=self.author_id, _external=True),
-            "comments" : url_for("api.get_post_comments", id=self.id, _external=True),
-            "comment_count" : self.comments.count()
+            "url": url_for("api.get_post", id=self.id, _external=True),
+            "body": self.body,
+            "body_html": self.body_html,
+            "timestamp": self.timestamp,
+            "author": url_for("api.get_user", id=self.author_id, _external=True),
+            "comments": url_for("api.get_post_comments", id=self.id, _external=True),
+            "comment_count": self.comments.count()
         }
         return json_post
 
