1f228cc4aa11356b02dc74b055a79116ade6606d
Rob Harriman <rmharriman@gmail.com>
2016-12-30 14:05:27 -0500

    Added updated requirements and a script to setup dev env on mac

---
+++ setup_mac.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+export MIGRATIONS=/Users/rmharriman/Documents/code/new_blog/migrations/
+export BLOG_HOME=/Users/rmharriman/Documents/code/new_blog
+export BLOG_VENV="$BLOG_HOME/bbb"
+export BLOG_ADMIN=rmharriman@gmail.com
+export DEV_DATABASE_URI=sqlite:////tmp/dev.db
+export TEST_DATABASE_URI=sqlite:////tmp/test.db
