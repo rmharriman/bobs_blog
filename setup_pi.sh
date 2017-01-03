895049c69c1b7368250b6809767e184290c6c653
Rob Harriman <rmharriman@gmail.com>
2017-01-02 19:40:30 -0500

    Shell script to setup production environment

---
+++ setup_pi.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+export BLOG_HOME=/srv/bobs_blog
+export BLOG_VENV=/srv/venv
+export BLOG_ADMIN=rmharriman@gmail.com
+export DEV_DATABASE_URI=sqlite:////tmp/dev.db
+export TEST_DATABASE_URI=sqlite:////tmp/test.db
+export DATABASE_URI=sqlite:////tmp/prod.db
