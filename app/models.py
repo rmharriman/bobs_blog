f8d1ba6fefe9b9c37e4ab33350c2c62fc2baa1d0
Rob <rob@uberresearch.com>
2017-01-10 10:35:18 -0500

    Fixed bug in post timestamp

--- app/models.py
+++ app/models.py
@@ -301,7 +301,7 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.Text)
     body_html = db.Column(db.Text)
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow())
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     comments = db.relationship("Comment", backref="post", lazy="dynamic")
 
